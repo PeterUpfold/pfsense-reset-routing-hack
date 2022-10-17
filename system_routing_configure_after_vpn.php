c594757308ad418362d65396da2f725c91e6ffb8
PeterUpfold <pgithub@upfold.org.uk>
2022-10-17 21:19:24 +0100

    Cut time to reconfigure routing

--- system_routing_configure_after_vpn.php
+++ system_routing_configure_after_vpn.php
@@ -2,9 +2,9 @@
 <?php
 require('/etc/inc/functions.inc');
 
-system("logger Will reconfigure routing in 3 mins");
+system("logger Will reconfigure routing in 45 seconds");
 
-sleep(180);
+sleep(45);
 
 system_routing_configure();
 system("logger Reconfigured routing");
