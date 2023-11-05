0366126f1941a3f389326d507747adb7b0252bdb
PeterUpfold <pgithub@upfold.org.uk>
2023-11-05 06:51:13 +0000

    also restart unbound

--- system_routing_configure_after_vpn.php
+++ system_routing_configure_after_vpn.php
@@ -1,11 +1,19 @@
 #!/usr/bin/env php
 <?php
-require('/etc/inc/functions.inc');
+require('/etc/inc/services.inc');
+require('/etc/inc/interfaces.inc');
+require('/etc/inc/unbound.inc');
+require('/etc/inc/service-utils.inc');
+define('WAIT_TIME', 60);
 
-system("logger Will reconfigure routing in 45 seconds");
+system('logger Will reconfigure routing in '. WAIT_TIME . ' seconds');
 
-sleep(45);
+sleep(WAIT_TIME);
 
 system_routing_configure();
 system("logger Reconfigured routing");
 
+// restart Unbound
+
+sync_unbound_service();
+service_control_restart('unbound', '');
